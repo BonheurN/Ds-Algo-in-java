e91c9df247f8436c223137a413429193c88ece78
Bonheur <ndamukunda098765@gmail.com>
2024-10-29 20:08:57 +0100

    Create Linked.java

---
+++ Linked.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Linked{
+  public static void main(String[]args){
+    LinkedList<String>linkedList = new String<String>();
+    linkedList.offer("A");
+    linkedList.offer("B"); // LinkedList can mimic stack or queue data structure it uses offer or push to add elements
+    linkedList.offer("C");
+    linkedList.offer("D");
+    linkedList.offer("F");
+
+    
+  }
+}
